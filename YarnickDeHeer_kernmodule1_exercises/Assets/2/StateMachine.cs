f0d3f2530834022496290cf3a71016e2423b52fc
yarnick <yarnickschool@gmail.com>
2020-09-06 20:21:12 +0200

    exercises yarnick

---
+++ YarnickDeHeer_kernmodule1_exercises/Assets/2/StateMachine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    private State CurrentS;
+    private Dictonary<System.Type,State> states = new Dictonary<System.Type,State>();
+     Start is called before the first frame update
+    void OnStart()
+    {
+       AddState(new IdleState(this));
+       AddState(new Attackstate(this));
+    }
+
+    // Update is called once per frame
+    void OnUpdate()
+    {
+        CurrentS?.OnUpdate();
+    }
+
+    public void SwitchState()
+    {
+        CurrentS?.OnExit();
+        CurrentS = states[type];
+        CurrentS?.OnEnter();
+    }
+
+    public void AddState()
+    {
+       states.Add(states.GetType(), state);
+    }
+}
