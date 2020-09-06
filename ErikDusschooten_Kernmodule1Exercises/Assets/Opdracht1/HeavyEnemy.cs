d2df096667545c640311ce4fa10d11381bb3f24a
Erikduss <erik.dusschooten@student.hku.nl>
2020-09-06 17:09:44 +0200

    Added my exercises to the git

---
+++ ErikDusschooten_Kernmodule1Exercises/Assets/Opdracht1/HeavyEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyEnemy : ActorBase
+{
+    public override void Move()
+    {
+    }
+
+    public override void Attack()
+    {
+    }
+
+    public override void TakeDamage()
+    {
+    }
+}
---
+++ ErikDusschooten_Kernmodule1Exercises/Assets/Opdracht1/IState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IState
+{
+    StateMachine owner { get; set; }
+    void OnUpdate();
+    void OnEnter();
+    void OnExit();
+}
