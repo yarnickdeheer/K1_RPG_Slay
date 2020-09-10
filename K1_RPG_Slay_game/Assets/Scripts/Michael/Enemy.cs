74f833510f581148c61c2fd8eec59e2b2914b172
mijkolsmith <mijkolsmith@live.com>
2020-09-10 17:35:11 +0200

    added enemy and player constructors

--- K1_RPG_Slay_game/Assets/Scripts/Michael/Enemy.cs
+++ K1_RPG_Slay_game/Assets/Scripts/Michael/Enemy.cs
@@ -1,6 +1,37 @@
 ﻿using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : IEnemy
 {
-    
+	//ICombatant Implementation
+	public int Level { get; set; }
+
+	public int Vit { get; set; }
+	public int Str { get; set; }
+	public int Dex { get; set; }
+
+	public int Health { get; set; }
+	public int WeightLimit { get; set; }
+	public int MovePoints { get; set; }
+
+	//IEnemy Implementation
+
+	//Constructor
+	public Enemy(int vit, int str, int dex, int weight)
+	{
+		Level = vit + str + dex;
+
+		Vit = vit;
+		Str = str;
+		Dex = dex;
+
+		Health = 10 + 2 * vit;
+		WeightLimit = 20 + 2 * str;
+		MovePoints = (int)Mathf.Floor(5 + dex / 5 - (weight - WeightLimit) / 10);
+	}
+
+	public void TakeDamage()
+	{
+
+	}
+	
 }
--- K1_RPG_Slay_game/Assets/Scripts/Michael/Player.cs
+++ K1_RPG_Slay_game/Assets/Scripts/Michael/Player.cs
@@ -1,18 +1,36 @@
-﻿public class Player : ICombatant
+﻿using UnityEngine;
+
+public class Player : IPlayer
 {
-	public int Health { get; set; }
+	//ICombatant Implementation
 	public int Level { get; set; }
+
 	public int Vit { get; set; }
 	public int Str { get; set; }
 	public int Dex { get; set; }
 
-	public Player(int health, int level, int vit, int str, int dex)
+	public int Health { get; set; }
+	public int WeightLimit { get; set; }
+	public int MovePoints { get; set; }
+
+	//IPlayer Implementation
+	public string PlayerClass { get; set; }
+
+	//Constructor
+	public Player(int vit, int str, int dex, string playerClass)
 	{
-		Health = health;
-		Level = level;
+		Level = vit + str + dex;
+
 		Vit = vit;
 		Str = str;
 		Dex = dex;
+		
+		Health = 10 + 2 * vit;
+		WeightLimit = 20 + 2 * str;
+		//This formula is incomplete and will not work correctly until I implement a method for getting the weapon and armor referenced in this class
+		MovePoints = (int) Mathf.Floor(5 + dex / 5 - (/*Weight - */WeightLimit) / 10);
+
+		PlayerClass = playerClass;
 	}
 
 	public void TakeDamage()
