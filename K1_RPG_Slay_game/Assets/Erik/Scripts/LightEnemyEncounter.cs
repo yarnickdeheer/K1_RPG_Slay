d37dc36afd39b556612b2590d3e1afb39f5e893e
Erikduss <erik.dusschooten@student.hku.nl>
2020-09-10 20:53:14 +0200

    Added basic code

---
+++ K1_RPG_Slay_game/Assets/Erik/Scripts/LightEnemyEncounter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEnemyEncounter : IMapEncounter, ISpawnable
+{
+    //Discuss: Most likely would be better to use inheritance for enemy encounter types since a lot of functions are (alsmost) the same.
+
+    //TODO: Double check code conventions.
+
+    public int Difficulty { get; set; }
+    private GameObject enemyMapObject;
+
+    public LightEnemyEncounter()
+    {
+        OnEnemyCreate();
+    }
+
+    public void OnSelect()
+    {
+        //TODO: Selection of encounter
+        Debug.Log("SELECTING LIGHT ENEMY");
+    }
+
+    public void OnDeselect()
+    {
+        Debug.Log("Deselecting LIGHT ENEMY");
+    }
+
+    public void PickSelection()
+    {
+        //TODO: Confirmation Selection of encounter
+        Debug.Log("PICKING LIGHT ENEMY FOR FIGHT");
+    }
+
+    private void OnEnemyCreate()
+    {
+        enemyMapObject = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/LightEnemyPrefab"));
+        DisableGameobject();
+    }
+
+    public void EnableGameobject(Vector2 position)
+    {
+        enemyMapObject.SetActive(true);
+        enemyMapObject.transform.position = position;
+    }
+
+    public void DisableGameobject()
+    {
+        enemyMapObject.SetActive(false);
+    }
+}
---
+++ K1_RPG_Slay_game/Assets/Erik/Scripts/MapDisplay.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay
+{
+    //DISCUSS: Should enemies have only 2 spawning positions? Or should the enemy spawning be expanded more?
+
+    private Vector2 _firstEcounterPosition = new Vector2(-3.5f,0);
+    private Vector2 _secondEcounterPosition = new Vector2(3.5f, 0);
+
+    private Vector2 _playerPosition = new Vector2(0, -3.5f);
+
+    private GameObject _playerObj;
+
+    private int _amountOfEnemiesInPool = 5;
+
+    //DISCUSS: Should enemies on the map be spawned from an object pool? Otherwise remove experimental code.
+    
+    /*private List<ISpawnable> _inactiveEnemyPool = new List<ISpawnable>();
+    private List<ISpawnable> _activeEnemyPool = new List<ISpawnable>();
+    
+
+    public void CreateEnemyPool()
+    {
+        for(int i = 0; i < _amountOfEnemiesInPool; i++)
+        {
+            _inactiveEnemyPool.Add(new LightEnemyEncounter());
+        }
+
+        for (int i = 0; i < _amountOfEnemiesInPool; i++)
+        {
+            _inactiveEnemyPool.Add(new HeavyEnemyEncounter());
+        }
+    }*/
+
+    public void PlacePlayer()
+    {
+        //DISCUSS: Will this be the way we instantiate the gameobjects? Or should we instantiate everything once and reuse it every time we get back to the map?
+        _playerObj = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/PlayerPrefab"));
+
+        _playerObj.transform.position = _playerPosition;
+    }
+
+    /*public void PlaceEnemies()
+    {
+        int random_1 = Random.Range(0, _inactiveEnemyPool.Count - 1);
+        PlaceEnemy(random_1, _firstEcounterPosition);
+
+        int random_2 = Random.Range(0, _inactiveEnemyPool.Count - 1);
+        PlaceEnemy(random_2, _secondEcounterPosition);
+    }
+
+    private void PlaceEnemy(int listID, Vector2 position)
+    {
+        _activeEnemyPool.Add(_inactiveEnemyPool[listID]);
+        _inactiveEnemyPool[listID].EnableGameobject(position);
+        _inactiveEnemyPool.RemoveAt(listID);
+    }*/
+
+    public void SpawnEnemies(List<IMapEncounter> enemies)
+    {
+        enemies[0].EnableGameobject(_firstEcounterPosition);
+        enemies[1].EnableGameobject(_secondEcounterPosition);
+    }
+}
