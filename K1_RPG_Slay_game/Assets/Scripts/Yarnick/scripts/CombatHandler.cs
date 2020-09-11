a876189889c999f6af934b8e1da6622bc9fb79e3
yarnick <yarnickschool@gmail.com>
2020-09-11 11:54:52 +0200

    op hoop van zegen

--- K1_RPG_Slay_game/Assets/Scripts/Yarnick/scripts/CombatHandler.cs
+++ K1_RPG_Slay_game/Assets/Scripts/Yarnick/scripts/CombatHandler.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class CombatHandler
 {
+    private int _TurnId;
+    private int _MovesLeft;
+    private bool _TurnConfirmed;
 
+    public void NextTurn()
+    {
+        throw new System.NotImplementedException();
+        // if current attacking player is done with his turn swap to the enemy
+        if (_TurnConfirmed == true)
+        {
+           // _TurnId = _turnIdNextCaracter;
+        }
+    }
+    public void EndCombat()
+    {
+        throw new System.NotImplementedException();
+        // if the player or the enemy is dead 
+        // when the player is dead go to post battle display as loser 
+        // when the enemy is dead go to post battle display as winner
+    }
 }
