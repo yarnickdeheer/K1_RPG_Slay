06f73b5c41844a379c6e064637101b7d9df05dc3
yarnick <yarnickschool@gmail.com>
2020-09-11 21:11:30 +0200

    update

--- K1_RPG_Slay_game/Assets/Yarnick/scripts/Display/PostBattleDisplay.cs
+++ K1_RPG_Slay_game/Assets/Yarnick/scripts/Display/PostBattleDisplay.cs
@@ -2,16 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class PostBattleDisplay
+using UnityEngine.UI;
+public class PostBattleDisplay : IdisplayItem
 {
-    private GameObject _Rewards;
+ 
+    /// <summary>
+    ///  this display wil show after the battle
+    /// check if you won
+    /// if so start generate rewards
+    /// else restart scene
+    ///
+    /// when the reward is generated get the rewards from the rewardchoice class
+    ///  
+    /// </summary>
+
+    public Image _ItemImage { get; set; }
+    public int _ItemId { get; set; }
+    
+    public IWeapon Weapon { get; set; }
+    public IPlayer Player { get; set; }
+
+    public int _Rewards;
     private bool _Won;
     private int _ExperienceGained;
     
+
+
+    public PostBattleDisplay( int _itemId , Image _itemImage ,IWeapon weapon,IPlayer player)
+    {
+        _ItemImage = _itemImage;
+        _ItemId = _itemId;
+        Weapon = weapon;
+        Player = player;
+    }
     public void BattleOutcome()
     {
-        throw new System.NotImplementedException();
+       // throw new System.NotImplementedException();
         // needs to know who won to decide if the player gets rewards
         if (_Won == true)
         {
@@ -26,7 +52,8 @@ public class PostBattleDisplay
     }
     public void GenerateRewards()
     {
-        throw new System.NotImplementedException();
+        
+        Player.GetXp(10);
         // let the player choose if it wants the reward that will be presented of keep the setup he has
     }
     
