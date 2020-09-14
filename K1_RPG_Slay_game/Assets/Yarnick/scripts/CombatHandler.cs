d8b8a7a9b6c2bdd06926ee765ada7f6b14168340
yarnick <yarnickschool@gmail.com>
2020-09-14 22:44:14 +0200

    new branch new code

--- K1_RPG_Slay_game/Assets/Yarnick/scripts/CombatHandler.cs
+++ K1_RPG_Slay_game/Assets/Yarnick/scripts/CombatHandler.cs
@@ -2,42 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatHandler : ICombatHandler
+public class CombatHandler : IcombatHandler
 {
     private int _MovesLeft;
     private bool _TurnConfirmed;
-    public bool _playerturn { get; set; }
-
-    public void StartTurn(){
+    public bool PlayerTurn { get; set; }
+    public GameManager _gameManager;
+    //public enemy _currentEnemy;
+    public void StartTurn()
+    {
         // we need to know speed of player and enemy to see who attacks first
-      ///  if (Player.MovePoints > Enemy.Moveponts)
-     ///   {
-      ///      _playerturn = true;
-      ///      NextTurn();
-     ///   }
-    ///    else
-    ///    {
-    ///        _playerturn = false;
-     ///       NextTurn();
-     ///   }
+
+        if (_gameManager.player.MovePoints > _gameManager.Currentenemy.MovePoints)
+        {
+            PlayerTurn = true;
+            NextTurn();
+        }
+        else
+        {
+            PlayerTurn = false;
+            NextTurn();
+        }
     }
     public void NextTurn()
     {
-        if (_playerturn == true)
+        if (PlayerTurn == true)
         {
             // set combat display aan 
-            _playerturn = false;
+            PlayerTurn = false;
             NextTurn();
-			
+
         }
         else
         {
             // Enemy AI start turn
-            _playerturn = true;
+            PlayerTurn = true;
             NextTurn();
         }
-      
-      
+
+
     }
     public void EndCombat()
     {
--- K1_RPG_Slay_game/Assets/Yarnick/scripts/Interfaces/IdisplayItem.cs
+++ K1_RPG_Slay_game/Assets/Yarnick/scripts/Interfaces/IdisplayItem.cs
@@ -1,11 +1,9 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-    public interface IdisplayItem
-    {
-         Image _ItemImage { get; set; }
-         int _ItemId { get; set; }
-    }
+public interface IdisplayItem
+{
+    Image ItemImage { get; set; }
+    int ItemId { get; set; }
+}
